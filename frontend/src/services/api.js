92850e88d39cb186a2b970bf935fb17e44919fa1
malla teja akash <you@example.com>
2026-05-14 18:52:50 +0530

    fixed api export issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
+
+export default API;
